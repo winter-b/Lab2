cfe2174e99e98f5e8443681d683a050a879589a6
winter-b <85021800+winter-b@users.noreply.github.com>
2021-12-20 22:43:20 +0200

    added unit and integration tests

--- app/src/androidTest/java/com/example/lab2/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/lab2/ExampleInstrumentedTest.java
@@ -1,13 +1,25 @@
 package com.example.lab2;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static org.hamcrest.CoreMatchers.allOf;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +29,30 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private String stringToBetyped;
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        stringToBetyped = "ass ass ass";
+    }
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.lab2", appContext.getPackageName());
+    }
+    @Test
+    public void CountingWordsPasses() {
+
+        onView(withId(R.id.edit_text))
+                .perform(typeText(stringToBetyped), closeSoftKeyboard());
+
+        onView(withId(R.id.button_send))            // withId(R.id.my_view) is a ViewMatcher
+                .perform(click());               // click() is a ViewAction
+        onView(withId(R.id.text_view)).check(matches(withText("3 Words")));
     }
 }
--- app/src/test/java/com/example/lab2/ExampleUnitTest.java
+++ app/src/test/java/com/example/lab2/ExampleUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.lab2;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +11,21 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    @Before
+    public void Setup(){
+    }
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void CountingWordsPasses() {
+        int count = StringService.CountWords("asdas dasdasd asdas");
+        assertEquals(3, count);
+    }
+    @Test
+    public void CountingSymbolsPasses() {
+        int count = StringService.CountSymbols("asdas dasd asdasdas!");
+        assertEquals(3, count);
     }
 }
